a7fdd2574843d2fa7a14fcaec0efef72fea89367
Robert <robert.garcia222@yahoo.com>
2024-07-12 19:03:10 -0500

    added tables column

--- api/src/migrations/1720828874599-create-tables.ts
+++ api/src/migrations/1720828874599-create-tables.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateTables1720826599775 implements MigrationInterface {
-    name = 'CreateTables1720826599775'
+export class CreateTables1720828874599 implements MigrationInterface {
+    name = 'CreateTables1720828874599'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "todo" ADD "title" character varying NOT NULL`);
